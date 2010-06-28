e869cc18a9e609f5123a2407dc69ff9667c256db
Ken Lee <kdblitz@blitz.localdomain>
2010-06-29 00:44:50 +0800

    Added label tags on login form

--- system/application/views/login.php
+++ system/application/views/login.php
@@ -18,7 +18,9 @@
     'size' => '50',
   );
   
+  echo form_label('Username',$username['id']);
   echo form_input($username);
+  echo form_label('Password',$password['id']);
   echo form_password($password);
   
   echo form_reset('reset','Reset');
