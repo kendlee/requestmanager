a18e8c37cff251736010b3187b23e1db814a2486
Ken Lee <kdblitz@blitz.localdomain>
2010-06-30 01:24:11 +0800

    fixed code

--- sql_setup/setup.sql
+++ sql_setup/setup.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE request_manager;
+CREATE DATABASE IF NOT EXISTS request_manager;
 USE request_manager;
-CREATE TABLE IF NOT EXISTS 'users' (
-  'id' INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  'username' VARCHAR( 64 ) NOT NULL ,
-  'password' VARCHAR( 64 ) NOT NULL ,
-  'real_name'
+CREATE TABLE IF NOT EXISTS users (
+  id INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+  username VARCHAR( 64 ) NOT NULL ,
+  password VARCHAR( 64 ) NOT NULL ,
+  real_name VARCHAR( 100 ) NOT NULL ,
   UNIQUE (
-    'username'
+    username
   )
 );
