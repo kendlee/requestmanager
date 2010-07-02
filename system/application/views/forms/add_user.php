b7cab4a6dd802a5e0f8bf8f908b924e5e1e78b41
Ken Lee <kdblitz@blitz.localdomain>
2010-07-02 19:46:22 +0800

    added stuff coded in school (resident form)

--- system/application/views/forms/add_user.php
+++ system/application/views/forms/add_user.php
@@ -40,21 +40,19 @@
     'value' => '1'
   );
   
+  $form_element = array (
+    form_label('Username',$username['id']).form_input($username),
+    form_label('Password',$password['id']).form_password($password),
+    form_label('Verify password',$verify['id']).form_password($password),
+    form_label('Real name',$real_name['id']).form_input($real_name),
+    form_fieldset('Account type').form_radio($type_encoder).form_label('Encoder',$type_encoder['id']).
+      form_radio($type_admin).form_label('Administrator',$type_admin['id']).form_fieldset_close()
+  );
   
-  echo form_label('Username',$username['id']);
-  echo form_input($username);
-  echo form_label('Password',$password['id']);
-  echo form_password($password);
-  echo form_label('Verify password',$verify['id']);
-  echo form_password($password);
-  echo form_label('Real name',$real_name['id']);
-  echo form_input($real_name);
-  echo form_fieldset('Account type');
-    echo form_radio($type_encoder);
-    echo form_label('Encoder',$type_encoder['id']);
-    echo form_radio($type_admin);
-    echo form_label('Administrator',$type_admin['id']);
-  echo form_fieldset_close();
+  foreach($form_element as $item) {
+    echo '<li>'.$item.'</li>';
+  }
+  
   echo form_reset('reset','Reset');
   echo form_submit('login','Register');
   echo form_close();
