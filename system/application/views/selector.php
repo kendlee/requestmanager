e0c52598405bb877c1c243ac506def838f083fdd
Ken Lee <kdblitz@blitz.localdomain>
2010-07-05 00:36:20 +0800

    changed a reference because of constants usage

--- system/application/views/selector.php
+++ system/application/views/selector.php
@@ -5,10 +5,10 @@
 <div id="selector">
   <h2>Categories</h2>
   <ul>
-    <li><a href="<?php echo site_url('manager/'.$sort_method.'/1');?>"><?=CATEGORY_1?></a></li>
-    <li><a href="<?php echo site_url('manager/'.$sort_method.'/2');?>"><?=CATEGORY_2?></a></li>
-    <li><a href="<?php echo site_url('manager/'.$sort_method.'/3');?>"><?=CATEGORY_3?></a></li>
-    <li><a href="<?php echo site_url('manager/'.$sort_method.'/4');?>"><?=CATEGORY_4?></a></li>
+    <li><a href="<?php echo site_url('manager/'.$sort_method.'/1');?>"><?=array_get('CATEGORY',1)?></a></li>
+    <li><a href="<?php echo site_url('manager/'.$sort_method.'/2');?>"><?=array_get('CATEGORY',2)?></a></li>
+    <li><a href="<?php echo site_url('manager/'.$sort_method.'/3');?>"><?=array_get('CATEGORY',3)?></a></li>
+    <li><a href="<?php echo site_url('manager/'.$sort_method.'/4');?>"><?=array_get('CATEGORY',4)?></a></li>
   </ul>
 </div>
 <div id="sorter">
